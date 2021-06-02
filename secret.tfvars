46784746d55a53acb6bc73a626d20abdd8b325db
Shivbir Kaur <shivbirkaur@amits-air.lan>
2021-06-02 16:41:45 -0400

    add

---
+++ secret.tfvars
@@ -0,0 +1,2 @@
+aws_access_key = "AKIA4ZPFYFG3PT5Z377Y"
+aws_secret_key = "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"
---
+++ security-gp.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP inbound connections"
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Allow HTTP Security Group"
+  }
+  }
